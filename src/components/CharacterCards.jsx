ea0d8ee9d71dec33a5a2762b165f311956e2857b
Ariyoadeyinka <ariyoadeyinka73@gmail.com>
2024-05-16 15:08:49 -0700

    new commit

--- src/components/CharacterCards.jsx
+++ src/components/CharacterCards.jsx
@@ -1,9 +1,21 @@
+import styles from "../css/CharacterCards.module.css";
 export default function CharacterCards({ bio }) {
   return (
     <div>
       <article>
-      <img src={bio?.images[0]} alt={bio?.name} />
-        <h1>{bio?.name}</h1>
+        <div>
+          <article className={styles.personalInfo}>
+            <img src={bio?.images[0]} alt={bio?.name} />
+            <h1>{bio?.name}</h1>
+            <p>Birthdate: {bio.personal.birthdate}</p>
+            <p>Sex: {bio.personal.sex}</p>
+            <p>Blood-Type: {bio.personal.bloodType}</p>
+            <strong>Affiliations:</strong>
+            {bio?.personal.affiliation?.map((affiliation, idx) => (
+              <li key={idx}>{affiliation}</li>
+            ))}
+          </article>
+        </div>
         <div>
           <h2>Jutsus</h2>
           <ol>
@@ -12,11 +24,20 @@ export default function CharacterCards({ bio }) {
             ))}
           </ol>
         </div>
+
         <div>
           <h2>Chakra Nautre</h2>
           <ol>
             {bio?.natureType?.map((natureType, idx) => (
               <li key={idx}>{natureType}</li>
+            ))}
+          </ol>
+        </div>
+        <div>
+          <h2>Family</h2>
+          <ol>
+            {bio?.father?.map((father, idx) => (
+              <li key={idx}>{father}</li>
             ))}
           </ol>
         </div>
