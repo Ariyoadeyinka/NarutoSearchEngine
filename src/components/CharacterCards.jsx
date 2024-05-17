4a29b1e1578a94616549b6fa59679c3de10ea10e
Ariyoadeyinka <ariyoadeyinka73@gmail.com>
2024-05-16 23:14:56 -0700

    new commit

--- src/components/CharacterCards.jsx
+++ src/components/CharacterCards.jsx
@@ -7,10 +7,18 @@ export default function CharacterCards({ bio }) {
           <article className={styles.personalInfo}>
             <img src={bio?.images[0]} alt={bio?.name} />
             <h3>{bio?.name}</h3>
-            <p><strong>Birthdate:</strong> {bio.personal.birthdate}</p>
-            <p><strong>Sex:</strong> {bio.personal.sex}</p>
-            <p><strong>Clan:</strong> {bio.personal.clan}</p>
-            <p><strong>Blood-Type:</strong> {bio.personal.bloodType}</p>
+            <p>
+              <strong>Birthdate:</strong> {bio.personal.birthdate}
+            </p>
+            <p>
+              <strong>Sex:</strong> {bio.personal.sex}
+            </p>
+            <p>
+              <strong>Clan:</strong> {bio.personal.clan}
+            </p>
+            <p>
+              <strong>Blood-Type:</strong> {bio.personal.bloodType}
+            </p>
             <strong>Affiliations:</strong>
             {bio?.personal.affiliation?.map((affiliation, idx) => (
               <li key={idx}>{affiliation}</li>
@@ -18,42 +26,59 @@ export default function CharacterCards({ bio }) {
           </article>
 
           <article className={styles.personalInfo}>
-            <img src={bio?.images[0]} alt={bio?.name} />
-            <h3>{bio?.name}</h3>
-            <p><strong>Birthdate:</strong> {bio.personal.birthdate}</p>
-            <p><strong>Sex:</strong> {bio.personal.sex}</p>
-            <p><strong>Clan:</strong> {bio.personal.clan}</p>
-            <p><strong>Blood-Type:</strong> {bio.personal.bloodType}</p>
-            <strong>Affiliations:</strong>
-            {bio?.personal.affiliation?.map((affiliation, idx) => (
-              <li key={idx}>{affiliation}</li>
-            ))}
+            <h3>🎙️VOICE ACTORS:</h3>
+            <p>
+              <strong>Japanese-VA:</strong> <br></br>
+              {bio.voiceActors.japanese}
+            </p>
+
+            <p>
+              <strong>English-VA:</strong>{" "}
+              {bio.voiceActors.english}
+            </p>
           </article>
         </div>
-        <div>
-          <h2>Jutsus</h2>
-          <ol>
-            {bio?.jutsu?.map((jutsu, idx) => (
-              <li key={idx}>{jutsu}</li>
-            ))}
-          </ol>
-        </div>
+        <div className={styles.OtherInfoBox}>
+          <article className={styles.OtherInfo}>
+            <h2>Jutsus</h2>
+            <ol>
+              {bio?.jutsu?.map((jutsu, idx) => (
+                <li key={idx}>{jutsu}</li>
+              ))}
+            </ol>
+          </article>
+          <div className={styles.OtherInfoOuter}>
+            {" "}
+            <article className={styles.OtherInfoInner}>
+              <h2>Chakra Nature</h2>
+              <ol>
+                {bio?.natureType?.map((natureType, idx) => (
+                  <li key={idx}>{natureType}</li>
+                ))}
+              </ol>
+            </article>
 
-        <div>
-          <h2>Chakra Nautre</h2>
-          <ol>
-            {bio?.natureType?.map((natureType, idx) => (
-              <li key={idx}>{natureType}</li>
-            ))}
-          </ol>
-        </div>
-        <div>
-          <h2>Family</h2>
-          <ol>
-            {bio?.father?.map((father, idx) => (
-              <li key={idx}>{father}</li>
-            ))}
-          </ol>
+            <article className={styles.OtherInfoInner}>
+              <h2>Character Debut</h2>
+              <p>
+              <strong>Manga Debut:</strong>{" "}
+              {bio.debut.manga}
+            </p>
+            <p>
+              <strong>Anime Debut:</strong>{" "}
+              {bio.debut.anime}
+            </p>
+            <p>
+              <strong>Novel Debut:</strong>{" "}
+              {bio.debut.novel}
+            </p>
+            <p>
+              <strong>Movie Debut:</strong>{" "}
+              {bio.debut.movie}
+            </p>
+
+            </article>
+          </div>
         </div>
       </article>
     </div>
