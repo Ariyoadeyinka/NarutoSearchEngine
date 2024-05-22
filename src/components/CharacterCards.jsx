19faf6bba7285bbe7e61e8fb071b91d042fd71f5
Ariyoadeyinka <ariyoadeyinka73@gmail.com>
2024-05-22 13:18:45 -0700

    new commit

--- src/components/CharacterCards.jsx
+++ src/components/CharacterCards.jsx
@@ -26,56 +26,60 @@ export default function CharacterCards({ bio }) {
               ))}
             </div>
           </article>
-      
-      
-          <article className={styles.personalInfo}>
-          <div className={styles.personalInfoContainer}>
-            <h3>🎙️VOICE ACTORS:</h3>
-            <p>
-              <strong>Japanese-VA:</strong> <br></br>
-              {bio.voiceActors.japanese}
-            </p>
 
-            <p>
-              <strong>English-VA:</strong> {bio.voiceActors.english}
-            </p>
+          <article className={styles.personalInfo}>
+            <div className={styles.personalInfoContainer}>
+              <h3>🎙️VOICE ACTORS:</h3>
+              <p>
+                <strong>Japanese-VA:</strong> <br></br>
+                {bio.voiceActors.japanese}
+              </p>
+
+              <p>
+                <strong>English-VA:</strong> {bio.voiceActors.english}
+              </p>
             </div>
           </article>
-         
         </div>
         <div className={styles.OtherInfoBox}>
           <article className={styles.OtherInfo}>
-            <h2>Jutsus</h2>
-            <ol>
-              {bio?.jutsu?.map((jutsu, idx) => (
-                <li key={idx}>{jutsu}</li>
-              ))}
-            </ol>
+            <div className={styles.personalInfoContainer}>
+              <h2>Jutsus</h2>
+              <ol>
+                {bio?.jutsu?.map((jutsu, idx) => (
+                  <li key={idx}>{jutsu}</li>
+                ))}
+              </ol>
+            </div>
           </article>
           <div className={styles.OtherInfoOuter}>
             {" "}
             <article className={styles.OtherInfoInner}>
-              <h2>Chakra Nature</h2>
-              <ol>
-                {bio?.natureType?.map((natureType, idx) => (
-                  <li key={idx}>{natureType}</li>
-                ))}
-              </ol>
+              <div className={styles.personalInfoContainer}>
+                <h2>Chakra Nature</h2>
+                <ol>
+                  {bio?.natureType?.map((natureType, idx) => (
+                    <li key={idx}>{natureType}</li>
+                  ))}
+                </ol>
+              </div>
             </article>
-            <article className={styles.OtherInfoInner}>
-              <h2>Character Debut</h2>
-              <p>
-                <strong>Manga Debut:</strong> {bio.debut.manga}
-              </p>
-              <p>
-                <strong>Anime Debut:</strong> {bio.debut.anime}
-              </p>
-              <p>
-                <strong>Novel Debut:</strong> {bio.debut.novel}
-              </p>
-              <p>
-                <strong>Movie Debut:</strong> {bio.debut.movie}
-              </p>
+            <article className={styles.OtherInfoInnerTwo}>
+              <div className={styles.personalInfoContainer}>
+                <h2>Character Debut</h2>
+                <p>
+                  <strong>Manga Debut:</strong> {bio.debut.manga}
+                </p>
+                <p>
+                  <strong>Anime Debut:</strong> {bio.debut.anime}
+                </p>
+                <p>
+                  <strong>Novel Debut:</strong> {bio.debut.novel}
+                </p>
+                <p>
+                  <strong>Movie Debut:</strong> {bio.debut.movie}
+                </p>
+              </div>
             </article>
           </div>
         </div>
