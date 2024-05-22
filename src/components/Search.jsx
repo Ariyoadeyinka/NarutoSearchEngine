3624eeeb39466a72660d2349085464ee334e7244
Ariyoadeyinka <ariyoadeyinka73@gmail.com>
2024-05-22 12:08:52 -0700

    new commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import CharacterCards from "./CharacterCards";
-import characters from '../images/Characters.png'
-import styles from '../css/Search.module.css'
+import characters from "../images/Characters.png";
+import styles from "../css/Search.module.css";
 
 export default function Search({ query, setQuery }) {
   const [bio, setBio] = useState({});
   const [loading, setLoading] = useState(false);
   const searchChar = async () => {
     if (!query) return;
-    setBio({})
+    setBio({});
     setLoading(true);
     try {
       const res = await fetch(
@@ -25,22 +25,30 @@ export default function Search({ query, setQuery }) {
   };
   return (
     <div>
-      <div className="container">
-      <img src={characters} alt="" />
-      <input
-        type="search"
-        name="search"
-        placeholder="Search"
-        aria-label="Search"
-        className={styles.Search}
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button onClick={searchChar} disabled={loading} aria-busy={loading ? "true" :  "false"}>
-        Search
-      </button>
+      <img src={characters} className={styles.charImage} alt="" />
+      <div className={styles.container}>
+        <input
+          type="search"
+          name="search"
+          placeholder="Search"
+          aria-label="Search"
+          className={styles.Search}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button
+          onClick={searchChar}
+          disabled={loading}
+          aria-busy={loading ? "true" : "false"}
+        >
+          Search
+        </button>
       </div>
-      {bio?.name?.length > 0? <CharacterCards bio = {bio} setBio={setBio} /> : ""}
+      {bio?.name?.length > 0 ? (
+        <CharacterCards bio={bio} setBio={setBio} />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
